d2334d25f97f9cd0c9f7e6b1962054f41b89a200
Mikhail Korol <93328012+Espirant0@users.noreply.github.com>
2024-09-06 14:18:12 +0200

    Исправлен маршрут для главной страницы

--- local/modules/bfu.garden/views/main.php
+++ local/modules/bfu.garden/views/main.php
@@ -5,6 +5,6 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Gardens");
 
-$APPLICATION->IncludeComponent('bfu:main', '', []);
+$APPLICATION->IncludeComponent('bfu:plots', '', []);
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");?>
